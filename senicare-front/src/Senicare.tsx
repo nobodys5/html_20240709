bbad279b35bfb18e68a685dd93e0dde396c9c258
nobodys5 <samudi2@naver.com>
2024-09-05 12:46:05 +0900

    수정

--- senicare-front/src/Senicare.tsx
+++ senicare-front/src/Senicare.tsx
@@ -4,6 +4,7 @@ import Auth from 'src/views/Auth';
 import { Route, Routes, useNavigate } from 'react-router';
 import MainLayout from './layouts/MainLaout';
 import { useCookies } from 'react-cookie';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CS_DETAIL_PATH, CS_PATH, CS_UPDATE_PATH, CS_WRITE_PATH, HR_DETAIL_PATH, HR_PATH, HR_UPDATE_PATH, MM_PATH, OTHER_PATH } from './constants';
 
 
 // component: root path 컴포넌트 //
@@ -17,8 +18,8 @@ function Index() {
 
   // effect: 마운트 시 경로 이동 effect //
   useEffect(() => {
-    if (cookies.accessToken) navigator('/cs');
-    else  navigator('/auth');
+    if (cookies[ACCESS_TOKEN]) navigator('/cs');
+    else  navigator(AUTH_ABSOLUTE_PATH);
   }, []);
 
   return (
@@ -31,25 +32,25 @@ export default function Senicare() {
   return (
     <Routes>
       <Route index element={<Index />} />
-      <Route path='/auth' element={<Auth />}/>
-      <Route path='/cs' element={<MainLayout />}>
+      <Route path={AUTH_PATH} element={<Auth />}/>
+      <Route path={CS_PATH} element={<MainLayout />}>
         <Route index element={<>고객 리스트 보기</>} />
-        <Route path='write' element={<>고객 등록</>} />
-        <Route path=':customNumber' element={<>고객 정보 보기</>} />
-        <Route path=':customNumber/update' element={<>고객 정보 수정</>} />
+        <Route path={CS_WRITE_PATH} element={<>고객 등록</>} />
+        <Route path={CS_DETAIL_PATH(':customNumber')} element={<>고객 정보 보기</>} />
+        <Route path={CS_UPDATE_PATH(':customNumber')} element={<>고객 정보 수정</>} />
       </Route>
 
-      <Route path='/mm' element={<MainLayout />}>
+      <Route path={MM_PATH} element={<MainLayout />}>
         <Route index element={<></>}/>
       </Route>
 
-      <Route path='/hr' element={<MainLayout />}>
+      <Route path={HR_PATH} element={<MainLayout />}>
         <Route index element={<></>}/>
-        <Route path=':userId' element={<></>}/>
-        <Route path=':userId/update' element={<></>}/>
+        <Route path={HR_DETAIL_PATH(':userId')} element={<></>}/>
+        <Route path={HR_UPDATE_PATH(':userId')} element={<></>}/>
       </Route>
 
-      <Route path='*' element={<Index />}/>
+      <Route path={OTHER_PATH} element={<Index />}/>
     </Routes>
   );
 }
--- senicare-front/src/layouts/MainLaout/index.tsx
+++ senicare-front/src/layouts/MainLaout/index.tsx
@@ -1,15 +1,86 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router'
+import './style.css';
+import { useCookies } from 'react-cookie';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, ROOT_ABSOLUTE_PATH } from 'src/constants';
+
+// component: 로고 컴포넌트 //
+function Logo() {
+
+  // render: 로고 컴포넌트 렌더링 //
+  return (
+    <div id='layout-logo'>
+      <div className='box'>
+        <div className='title'>시니케어</div>
+        <div className='icon'></div>
+      </div>
+    </div>
+  )
+}
+
+// component: 상단 컴포넌트 //
+function Top() {
+
+  // state: path 상태 //
+  const { pathname } = useLocation();
+  // state: cookie 상태 //
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  
+  // variable: 경로 이름 //
+  const path = 
+  pathname.startsWith('/cs') ? '고객 관리' : 
+  pathname.startsWith('/mm') ? '용품 관리' : 
+  pathname.startsWith('/hr') ? '인사 관리' : ''; 
+
+  // function: 네비게이터 함수 //
+  // 위치를 찾는 useHook함수
+  const navigator = useNavigate();
+
+  // event handler: 로그아웃 버튼 클릭 이벤트 처리 //
+  const onLogoutButtonClickHandler = () => {
+    removeCookie('accessToken', { path: ROOT_ABSOLUTE_PATH });
+    navigator(AUTH_ABSOLUTE_PATH);
+  };
+  
+  // render: 상단 컴포넌트 //
+  return (
+    <div id='layout-top'>
+      <div className='path'>{path}</div>
+      <div className='button second' onClick={onLogoutButtonClickHandler}>로그아웃</div>
+    </div>
+  )
+}
+
+// component: 좌측 네비게이션 컴포넌트 /
+function SideNavigation() {
+
+    // render: 좌측 네비게이션 컴포넌트 //
+    return (
+      <div id='layout-side-navigation'></div>
+    );
+}
 
 // component: 메인 레이아웃 컴포넌트 //
 export default function MainLayout() {
 
+  // state: 쿠키 상태 //
+  const [cookies] = useCookies();
+  
+  // function : 네비게이터 함수 //
+  const navigator = useNavigate();
+  
+  // effect: 레이아웃 마운트시 로그인 여부 확인 //
+  useEffect(() => {
+      if (!cookies[ACCESS_TOKEN]) navigator(AUTH_ABSOLUTE_PATH);
+  }, []);
+
   // render: 메인 레이아웃 컴포넌트 렌더링 //
   return (
-    <div>
-        <div>로고</div>
-        <div>상단</div>
-        <div>좌측 네비게이션</div>
+    <div id='main-layout'>
+        <Logo />
+        <Top />
+        <SideNavigation />
         <Outlet />
     </div>
   )
